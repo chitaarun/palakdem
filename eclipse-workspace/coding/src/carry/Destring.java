19aa72ee7fa72f2e52286de21c308cd5a531f944
Chitaarun <chitaarun@gmail.com>
2020-06-09 13:00:04 -0400

    They are commit

---
+++ eclipse-workspace/coding/src/carry/Destring.java
@@ -0,0 +1,35 @@
+package carry;
+
+public class Destring {
+
+	public static void main(String[] args) {
+		
+	
+		int days = 7;
+		
+		if(days==1) {
+			System.out.println("this is sunday:" );
+		}
+		if(days==2) {
+			System.out.println("this is monday:"  );
+		}
+		if(days==3) {
+			System.out.println("this is tuesday:"  );
+		}
+		if(days==4) {
+			System.out.println("this is wednesday:"  );
+		}
+		if(days==5) {
+			System.out.println("this is thrusday:"  );
+		}
+		if(days==6) {
+			System.out.println("this is fri day:"  );
+		}
+		if(days==7) {
+			System.out.println("this is sature day:" );
+		}else {
+			System.out.println("this is wrong day");
+		}
+	}
+
+}
